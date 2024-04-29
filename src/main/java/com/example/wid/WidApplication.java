d2651906a5f38caf8c3076365de10a003e8eaadd
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2024-04-29 20:07:02 +0900

    init:WID init

---
+++ src/main/java/com/example/wid/WidApplication.java
@@ -0,0 +1,13 @@
+package com.example.wid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wid/WidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
