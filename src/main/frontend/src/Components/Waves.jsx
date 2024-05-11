890c46fc9bc2886e2dc41cb6a40937c251e8a0f4
DangerousLittleBear <youngbin0817@gmail.com>
2024-05-11 10:42:22 +0900

    배경화면을 감싸고 있는 파도 컴포넌트를 단순 CSS에서 자바스크립트로 변경하는 작업

--- src/main/frontend/src/Components/Waves.jsx
+++ src/main/frontend/src/Components/Waves.jsx
@@ -1,28 +1,9 @@
-import React, {useState} from 'react';
-import styled,{keyframes} from 'styled-components';
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
-
-// 스타일드 컴포넌트 사용하여 스타일 정의
-const WavesContainer = styled.div`
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 50%; 
-    margin-bottom: -7px; /* Safari 갭 해결을 위한 수정 */
-`;
-
-const ParallaxWave = styled.svg`
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-`;
-
-// Waves 컴포넌트 정의
 const Waves = () => {
     return (
-        <WavesContainer>
+        <WavesContainer className="waves">
             <ParallaxWave xmlns="http://www.w3.org/2000/svg" viewBox="0 24 150 28" preserveAspectRatio="none">
                 <defs>
                     <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>
@@ -37,5 +18,58 @@ const Waves = () => {
         </WavesContainer>
     );
 };
+
+const moveForever = keyframes`
+    0% {
+        transform: translate3d(-90px, 0, 0);
+    }
+    100% {
+        transform: translate3d(85px, 0, 0);
+    }
+`;
+
+const WavesContainer = styled.div`
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 50vh;
+    margin-bottom: -7px; /*Fix for safari gap*/
+    min-height: 0%;
+    max-height: %;
+    overflow: hidden;
+    z-index: -2;
+`;
+
+const ParallaxWave = styled.svg`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    .parallax > use {
+        animation: ${moveForever} 25s cubic-bezier(0.55,.5,0.45,.5) infinite;
+    }
+
+    .parallax > use:nth-child(1) {
+        animation-delay: -2s;
+        animation-duration: 49s;
+    }
+
+    .parallax > use:nth-child(2) {
+        animation-delay: -3s;
+        animation-duration: 56s;
+    }
+
+    .parallax > use:nth-child(3) {
+        animation-delay: -4s;
+        animation-duration: 55s;
+    }
+
+    .parallax > use:nth-child(4) {
+        animation-delay: -5s;
+        animation-duration: 60s;
+    }
+`;
 
 export default Waves;
--- src/main/frontend/src/Pages/UserCreateVP.jsx
+++ src/main/frontend/src/Pages/UserCreateVP.jsx
@@ -1,2 +1,4 @@
 import React, { useState } from 'react';
 import styled,{keyframes} from 'styled-components';
+import Waves from '../Components/Waves';
+import Button from '../Components/Button';
