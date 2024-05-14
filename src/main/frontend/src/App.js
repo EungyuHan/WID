6e282c60b54ff29b12aea7d0899fa4f1804aa85b
DangerousLittleBear <youngbin0817@gmail.com>
2024-05-14 15:24:42 +0900

    VP생성을 위한 페이지 작업중 (작업 url은 Working)

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,6 +4,7 @@ import UserMainPage from "./Pages/UserMainPage";
 import { BrowserRouter ,Route, Routes} from 'react-router-dom';
 import CheckPrivateModal from "./Components/CheckPrivateModal";
 import UserCreateVC from "./Pages/UserCreateVC";
+import UserCreateVP from "./Pages/UserCreateVP";
 
 function App() {
   return (
@@ -11,8 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Loginpage />}></Route>
         <Route path='/MainPage' element={<UserMainPage/>}></Route>
-        <Route path='/Something' element={<UserCreateVC/>}></Route>
-        <Route path='/Working' element={<UserMainPage/>}></Route>
+        <Route path='/CreateVC' element={<UserCreateVC/>}></Route>
+        <Route path='/Working' element={<UserCreateVP/>}></Route>
       </Routes>
     </BrowserRouter>
       
--- src/main/frontend/src/Pages/UserCreateVP.jsx
+++ src/main/frontend/src/Pages/UserCreateVP.jsx
@@ -2,3 +2,124 @@ import React, { useState } from 'react';
 import styled,{keyframes} from 'styled-components';
 import Waves from '../Components/Waves';
 import Button from '../Components/Button';
+import {Link} from 'react-router-dom';
+
+function UserCreateVP() {
+    const [item, setItem] = useState([]);
+    const itemList = [];
+
+    const addItem = () => {
+        
+    }
+
+    return(
+        <BackGround>
+            <div div style={{ zIndex: 1 }}>
+                <LogoContainer>
+                <img src='img/logo.png' width={`80px`} height={`80px`} alt='Logo'></img>
+                </LogoContainer>
+                <HomeContainer>
+                <Link to="/MainPage">
+                        <img src='img/Home.png' width={`60px`} height={`60px`} alt='Logo'></img>
+                        </Link>
+                </HomeContainer>
+                <HelpContainer>
+                <Link to="/Help">
+                        <img src='img/Help.png' width={`75x`} height={`75px`} alt='Logo'></img>
+                        </Link>
+                </HelpContainer>
+
+                <SelectBarDiv>
+                    <ItemDiv>
+                        
+                    </ItemDiv>
+                    <ItemAddButtonDiv>
+                        <Button name="항목추가하기" onClick={addItem}></Button>
+                    </ItemAddButtonDiv>
+                    
+                </SelectBarDiv>
+
+                <ContentDiv>
+
+                </ContentDiv>
+            </div>
+            <Waves/>
+        </BackGround>
+    )
+}
+
+
+
+const BackGround = styled.div`
+    position: fixed;
+    background: linear-gradient(to right, #FFFFFF, #0083b0);
+    width: 100%;
+    height: 100vh;
+    padding = 0px;
+    display: flex;
+`
+const LogoContainer = styled.div`
+    position: fixed; 
+    width: 60%;
+    height: 12%;
+    left: 5%;
+    background-color: transparent;
+`
+
+const HomeContainer = styled.div`
+    position: fixed;
+    width: 10%;
+    height: 12%;
+    top:1%;
+    right:15%;
+`
+const HelpContainer = styled.div`
+    position: fixed;
+    width: 10%;
+    height: 12%;
+    right:5%;
+`
+const SelectBarDiv = styled.div`
+    position: fixed;
+    top : 12%;
+    left : 3%;
+    width: 20%;
+    height: 80%;
+    border-radius : 15px;
+    background-color: rgba(0, 0, 0, 0.25);
+`
+
+const ContentDiv = styled.div`
+    position: fixed;
+    top : 12%;
+    left : 27%;
+    width: 70%;
+    height: 85%;
+    border-radius : 15px;
+    background-color: rgba(0, 0, 0, 0.25);
+`
+
+const ItemDiv = styled.div`
+    position: relative;
+    top: 3%;
+    width:90%;
+    height:80%;
+    margin: auto;
+    border-radius: 10px;
+    display:block;
+    background-color: #ffffffcc;
+    overflow:scroll;
+`
+const ItemAddButtonDiv = styled.div`
+    position: relative;
+    top:5%;
+    width:90%;
+    height:10%;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+
+export default UserCreateVP;
